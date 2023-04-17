7d4dcc5de447586fcb0c90527d7c829e8142bed5
Abelardo Andres Bacar <abacar@edrans.com>
2023-04-17 16:36:14 -0300

    Point backend to the correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "MY-Terraform-Demos"
 
     workspaces {
       name = "learn-terraform-github-actions"
